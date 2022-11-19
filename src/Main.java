ec7fc909bd88de8dbc0bb245514a5458cc50fa90
Salymbek <calumbekcaatbekov@gmail.com>
2022-11-19 16:15:53 +0600

    commit

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,43 @@
+import packpage1.Week;
+
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner scanner = new Scanner(System.in);
+        String week = scanner.nextLine();
+
+
+        switch (week){
+            case "MONDAY":
+                System.out.println( Week.MONDAY.getDesignation());
+                break;
+            case "TUESDAY":
+                System.out.println( Week.TUESDAY.getDesignation());
+                break;
+            case "WEDNESDAY":
+                System.out.println(Week.WEDNESDAY.getDesignation());
+                break;
+            case "THURSDAY":
+                System.out.println(Week.THURSDAY.getDesignation());
+                break;
+            case "FRIDAY":
+                System.out.println(Week.FRIDAY.getDesignation());
+                break;
+            case "SATURDAY":
+                System.out.println(Week.SATURDAY.getDesignation());
+                break;
+            case "SUNDAY":
+                System.out.println(Week.SUNDAY.getDesignation());
+                break;
+            default:
+                System.out.println("МЫНДАЙ КУН ЖОК");
+                break;
+        }
+
+
+
+
+
     }
 }
--- src/packpage1/Week.java
+++ src/packpage1/Week.java
@@ -1,4 +1,25 @@
 package packpage1;
+public enum Week {
 
-public enum Wee {
+    MONDAY("Дуйшомбу куну technical lesson окуйм"),
+    TUESDAY("Шейшемби куну англис тили сабагын окуйм"),
+    WEDNESDAY("Шаршемби куну soft skills сабагын окуйм"),
+    THURSDAY("Бейшемби куну session кором"),
+    FRIDAY("Жума куну event ке катышам"),
+    SATURDAY("Ишемби куну дем алыш"),
+    SUNDAY("Жекшемби куну дем алыш");
+
+    private String designation;
+
+    Week(String designation) {
+        this.designation = designation;
+    }
+
+    public String getDesignation() {
+        return designation;
+    }
+
+    public void setDesignation(String designation) {
+        this.designation = designation;
+    }
 }
